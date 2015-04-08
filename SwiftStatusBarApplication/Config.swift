d28e1cc6288886c22f010ca1bca73887a1626f45
Christian Schulze <christian.schulze@mni.thm.de>
2015-04-08 12:08:42 +0200

    added doc for config file

--- SwiftStatusBarApplication/Config.swift
+++ SwiftStatusBarApplication/Config.swift
@@ -8,6 +8,16 @@
 
 import Foundation
 
+/**
+    This structure provides mandatory configurations like secrets or API routes.
+*/
 struct Config {
+    /**
+        This is the base URL for the idrop.link API
+    
+        :warning: If you want do deploy a copy of idrop.link on your own you
+                  have to edit this path to match your base link of your 
+                  backend copy.
+    */
     static let baseURL = "https://api.idrop.link/v1"
 }
