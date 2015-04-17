f09eda17fb98f80efc3b84f3924f650782747669
Christian Schulze <christian.schulze@mni.thm.de>
2015-04-17 14:20:33 +0200

    added security framework and added keychain class stub

---
+++ SwiftStatusBarApplication/Keychain.swift
@@ -0,0 +1,14 @@
+//
+//  Keychain.swift
+//  SwiftStatusBarApplication
+//
+//  Created by Christian Schulze on 17/04/15.
+//  Copyright (c) 2015 Tommy Leung. All rights reserved.
+//
+
+import Foundation
+import Security
+
+public class Keychain {
+    // TODO
+}
