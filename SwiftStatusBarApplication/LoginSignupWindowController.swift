3a290944a4fdf82dfca37e0fcdde60a2aa3bd6aa
Christian Schulze <christian.schulze@mni.thm.de>
2015-04-18 17:35:28 +0200

    see 78628c1295fd1a5a781dfedd57181275a223a12a

--- SwiftStatusBarApplication/LoginSignupWindowController.swift
+++ SwiftStatusBarApplication/LoginSignupWindowController.swift
@@ -13,7 +13,13 @@ class LoginSignupWindowController: NSWindowController {
     @IBOutlet var _window: NSWindow!
     @IBOutlet var loginSheet: NSPanel!
     @IBOutlet var signupSheet: NSPanel!
-
+    
+    @IBOutlet weak var signupEmail: NSTextField!
+    @IBOutlet weak var signupPassword: NSTextField!
+    
+    var user: User?
+    
+    // MARK: - Login
     @IBAction func showLoginSheet(sender: AnyObject) {
         _window.beginSheet(loginSheet, completionHandler: nil)
     }
@@ -23,8 +29,26 @@ class LoginSignupWindowController: NSWindowController {
         loginSheet.orderOut(sender)
     }
     
+    // MARK: - Signup
     @IBAction func showSignupSheet(sender: AnyObject) {
         _window.beginSheet(signupSheet, completionHandler: nil)
+    }
+    
+    @IBAction func doSignup(sender: AnyObject) {
+        print("do signup\n")
+        if let usr = self.user {
+            usr.createUser(self.signupEmail.stringValue, password: self.signupPassword.stringValue, callback: { (success, msg) in
+                if (success) {
+                    print("id: \(msg)")
+                    self.closeSignupSheet(self)
+                } else {
+                    print("failed\n")
+                    print("\(msg)\n")
+                }
+            })
+        } else {
+            print("fuck, no user\n")
+        }
     }
     
     @IBAction func closeSignupSheet(sender: AnyObject) {
