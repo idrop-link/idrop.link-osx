c9da5a2c342f79a26cd9d30ddc5bf0cd1d9705cf
Christian Schulze <christian.schulze@mni.thm.de>
2015-04-28 12:28:59 +0200

    added separate login window and controller

---
+++ idrop.link/LoginWindowController.swift
@@ -0,0 +1,19 @@
+//
+//  Login.swift
+//  idrop.link
+//
+//  Created by Christian Schulze on 28/04/15.
+//  Copyright (c) 2015 andinfinity. All rights reserved.
+//
+
+import Cocoa
+
+class Login: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+}
